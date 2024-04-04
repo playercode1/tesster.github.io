738a8bd9ae7f28be202895c6bbe52411ca4c12a6
playercode1 <165576479+playercode1@users.noreply.github.com>
2024-04-04 19:59:46 +0700

    Create tester.cpp

---
+++ tester.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	double x = 3;
+	double y = 2;
+	
+	
+	cout << "x = " <<x;
+	cout << "\n y = " <<y;
+	cout << "\n total = " << x*y; 
+	cout << "\n iec cream = " <<x-y;
+	cout << "\n beer = " <<x+y;
+	cout << "\n coke = " <<y  * y + ++x;
+	cout << "\n water = " << x + y + ++x *y;
+	cout << "\n locker = " << x / y;
+	
+		return 0;
+	
+}
